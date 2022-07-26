6190ca70b4b93cb87a6d89b92406bae7fd94f873
heyazoo1007 <heyazoo1007@gmail.com>
2022-07-26 15:48:35 +0900

    second commit

---
+++ src/main/java/practice/localdeploy/LocaldeployApplication.java
@@ -0,0 +1,13 @@
+package practice.localdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaldeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaldeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/localdeploy/LocaldeployApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.localdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaldeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
