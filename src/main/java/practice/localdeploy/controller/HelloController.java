5010b8359bfee2a1b3b7400f34e0008b8d99ba06
heyazoo1007 <heyazoo1007@gmail.com>
2022-09-06 20:04:38 +0900

    [FIX] gitignore 수정

--- src/main/java/practice/localdeploy/controller/HelloController.java
+++ src/main/java/practice/localdeploy/controller/HelloController.java
@@ -2,25 +2,20 @@ package practice.localdeploy.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import practice.localdeploy.service.HelloService;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/hello")
+
 @RequiredArgsConstructor
 public class HelloController {
 
     private final HelloService helloService;
 
-    @PostMapping("/save")
-    public HttpStatus save(@RequestBody HelloRequest helloRequest, HttpServletResponse response) {
-        String name = helloRequest.getName();
-        helloService.save(name);
-        return HttpStatus.valueOf(200);
+    @RequestMapping("/hello")
+    public String sample() {
+        return "Sample";
     }
 }
